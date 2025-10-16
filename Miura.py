f3679d8c2bd268f779df632b6625b6e5f73bc399
Habibullo39 <zubaidullozoda.habibullo.22@gmail.com>
2025-10-16 16:39:54 +0500

    Add files via upload

---
+++ Miura.py
@@ -0,0 +1,32 @@
+howmany = int(input("how many words do you wanna write"))
+words = []
+i = 0
+while i < howmany:
+    mes = input("write a word")
+    words.append(mes)
+    i = i + 1
+
+isitover = 0
+
+i = 0
+while isitover == 0:
+    j = 1
+    while j < len(words) and isitover == 0:
+        first = words[0]
+        another = words[j]
+        if i < len(first) and i < len(another):
+         if first[i] == another[i]:
+            j = j + 1
+         else:
+            isitover = 1
+        else:
+           isitover = 1
+
+    if isitover == 0:
+      letter = first[i]
+      print(letter,end="")
+    else:
+       if i == 0:
+          print("No common prefix")
+
+    i = i + 1
